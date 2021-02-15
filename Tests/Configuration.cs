ff8444bda80c6d84a3eff05f1d299e29c1e4f75a
Домашних Иван <digi@skbkontur.ru>
2021-02-15 19:07:04 +0500

    Fixed ports

--- Tests/Configuration.cs
+++ Tests/Configuration.cs
@@ -2,6 +2,6 @@ namespace Tests
 {
     public static class Configuration
     {
-        public static string BaseUrl => "http://samplehost:777";
+        public static string BaseUrl => "https://localhost:5001";
     }
 }
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,5 +1,5 @@
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace WebApi
 {
@@ -7,11 +7,20 @@ namespace WebApi
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();
         }
+        
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    // NOTE: Жесткий способ настройки, который сработает в 100% различных IDE.
+                    // Для продакшена следует использовать аргументы командной строки,
+                    // переменные окружения, файлы конфигурации
+                    webBuilder.UseUrls("https://localhost:5001;http://localhost:5000");
+                    webBuilder.UseEnvironment("Development");
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
