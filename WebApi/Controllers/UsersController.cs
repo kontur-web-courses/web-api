edc5369c7051aa3eff950babbecd113a6ec10001
Домашних Иван <digi@skbkontur.ru>
2021-02-16 13:22:17 +0500

    Solved 1

--- WebApi/Controllers/UsersController.cs
+++ WebApi/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using AutoMapper;
 using Game.Domain;
 using Microsoft.AspNetCore.Mvc;
 using WebApi.Models;
@@ -9,15 +10,26 @@ namespace WebApi.Controllers
     [ApiController]
     public class UsersController : Controller
     {
+        private readonly IUserRepository userRepository;
+        private readonly IMapper mapper;
+        
         // Чтобы ASP.NET положил что-то в userRepository требуется конфигурация
-        public UsersController(IUserRepository userRepository)
+        public UsersController(IUserRepository userRepository, IMapper mapper)
         {
+            this.userRepository = userRepository;
+            this.mapper = mapper;
         }
 
-        [HttpGet("{userId}")]
+        [HttpGet("{userId}", Name = nameof(GetUserById))]
+        [Produces("application/json", "application/xml")]
         public ActionResult<UserDto> GetUserById([FromRoute] Guid userId)
         {
-            throw new NotImplementedException();
+            var userFromRepo = userRepository.FindById(userId);
+            if (userFromRepo == null)
+                return NotFound();
+
+            var userDto = mapper.Map<UserDto>(userFromRepo);
+            return Ok(userDto);
         }
 
         [HttpPost]
