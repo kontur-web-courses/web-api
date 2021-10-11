8143de077cb91efdbc3451f785188ce7426712c5
d.stukov <d.stukov@skbkontur.ru>
2021-10-11 17:13:07 +0500

    Task 1. Get user by id

--- WebApi/Controllers/UsersController.cs
+++ WebApi/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using AutoMapper;
 using Game.Domain;
 using Microsoft.AspNetCore.Mvc;
 using WebApi.Models;
@@ -7,17 +8,28 @@ namespace WebApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Produces("application/json", "application/xml")]
     public class UsersController : Controller
     {
+        private IUserRepository repository;
+        private IMapper mapper;
+        
         // Чтобы ASP.NET положил что-то в userRepository требуется конфигурация
-        public UsersController(IUserRepository userRepository)
+        public UsersController(IUserRepository repository, IMapper mapper)
         {
+            this.repository = repository;
+            this.mapper = mapper;
         }
 
         [HttpGet("{userId}")]
         public ActionResult<UserDto> GetUserById([FromRoute] Guid userId)
         {
-            throw new NotImplementedException();
+            var user = repository.FindById(userId);
+
+            if (user == null)
+                return NotFound();
+
+            return Ok(mapper.Map<UserDto>(user));
         }
 
         [HttpPost]
--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -1,7 +1,13 @@
+using System;
+using System.Reflection;
+using AutoMapper;
+using Game.Domain;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using WebApi.Models;
 
 namespace WebApi
 {
@@ -17,11 +23,28 @@ namespace WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers()
+            services.AddControllers(options =>
+                {
+                    // Этот OutputFormatter позволяет возвращать данные в XML, если требуется.
+                    options.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+                    // Эта настройка позволяет отвечать кодом 406 Not Acceptable на запросы неизвестных форматов.
+                    options.ReturnHttpNotAcceptable = true;
+                    // Эта настройка приводит к игнорированию заголовка Accept, когда он содержит */*
+                    // Здесь она нужна, чтобы в этом случае ответ возвращался в формате JSON
+                    options.RespectBrowserAcceptHeader = true;
+                })
                 .ConfigureApiBehaviorOptions(options => {
                     options.SuppressModelStateInvalidFilter = true;
                     options.SuppressMapClientErrors = true;
                 });
+
+            services.AddSingleton<IUserRepository, InMemoryUserRepository>();
+            
+            services.AddAutoMapper(cfg =>
+            {
+                cfg.CreateMap<UserEntity, UserDto>()
+                    .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => $"{src.LastName} {src.FirstName}"));
+            }, Array.Empty<Assembly>());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
