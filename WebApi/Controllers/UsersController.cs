3aa9e13eb2cccf0be9d6c7bc773780e9e372668c
kryaazh <57125074+kryaazh@users.noreply.github.com>
2021-10-11 19:44:44 +0500

    Task 1—4 done

--- WebApi/Controllers/UsersController.cs
+++ WebApi/Controllers/UsersController.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Linq;
+using AutoMapper;
 using Game.Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using WebApi.Models;
 
@@ -10,20 +14,96 @@ namespace WebApi.Controllers
     public class UsersController : Controller
     {
         // Чтобы ASP.NET положил что-то в userRepository требуется конфигурация
-        public UsersController(IUserRepository userRepository)
+        private readonly IUserRepository userRepository;
+        private readonly IMapper mapper;
+        public UsersController(IUserRepository userRepository, IMapper mapper)
         {
+            this.userRepository = userRepository;
+            this.mapper = mapper;
         }
 
-        [HttpGet("{userId}")]
+        [HttpGet("{userId}", Name = nameof(GetUserById))]
+        [Produces("application/json", "application/xml")]
         public ActionResult<UserDto> GetUserById([FromRoute] Guid userId)
         {
-            throw new NotImplementedException();
+            var userFromRepository = userRepository.FindById(userId);
+            if (userFromRepository == null)
+                return NotFound();
+
+            var userDto = mapper.Map<UserDto>(userFromRepository);
+            return Ok(userDto);
         }
 
         [HttpPost]
-        public IActionResult CreateUser([FromBody] object user)
+        [Produces("application/json", "application/xml")]
+        public IActionResult CreateUser([FromBody] UserToCreate user)
         {
-            throw new NotImplementedException();
+            if (user == null)
+                return BadRequest();
+            
+            if (string.IsNullOrEmpty(user.Login) || !user.Login.All(char.IsLetterOrDigit))
+                ModelState.AddModelError(nameof(UserToCreate.Login),"Логин должен содержать только буквы или цифры");
+
+            if (!ModelState.IsValid)
+                return UnprocessableEntity(ModelState);
+            
+            var userEntity = mapper.Map<UserEntity>(user);
+            var createdUserEntity = userRepository.Insert(userEntity);
+            
+            return CreatedAtRoute(
+                nameof(GetUserById),
+                new { userId = createdUserEntity.Id},
+                createdUserEntity.Id);
+        }
+
+        [HttpPut("{userId}")]
+        [Produces("application/json", "application/xml")]
+        public IActionResult UpdateUser([FromRoute] Guid userId, [FromBody] UserToUpdate user)
+        {
+            if (user == null || userId == Guid.Empty)
+                return BadRequest();
+
+            if (!ModelState.IsValid)
+                return UnprocessableEntity(ModelState);
+
+            var userEntity = new UserEntity(userId);
+            mapper.Map(user, userEntity);
+            userRepository.UpdateOrInsert(userEntity, out bool isInserted);
+
+            if (isInserted)
+            {
+                return CreatedAtRoute(
+                    nameof(GetUserById),
+                    new {userId = userEntity.Id},
+                    userEntity.Id);
+            }
+
+            return NoContent();
+        }
+
+        [HttpPatch("{userId}")]
+        [Produces("application/json", "application/xml")]
+        public IActionResult PartiallyUpdateUser([FromRoute] Guid userId, [FromBody] JsonPatchDocument<UserToUpdate> patchDoc)
+        {
+            if (patchDoc == null)
+                return BadRequest();
+            
+            var userFromRepository = userRepository.FindById(userId);
+            if (userFromRepository == null)
+                return NotFound();
+
+            var user = mapper.Map<UserToUpdate>(userFromRepository);
+
+            patchDoc.ApplyTo(user, ModelState);
+            TryValidateModel(user);
+
+            if (!ModelState.IsValid)
+                return UnprocessableEntity(ModelState);
+
+            mapper.Map(user, userFromRepository);
+            userRepository.Update(userFromRepository);
+
+            return NoContent();
         }
     }
 }
--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -1,7 +1,14 @@
+using AutoMapper;
+using Game.Domain;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.DataCollection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using WebApi.Models;
 
 namespace WebApi
 {
@@ -17,11 +24,34 @@ namespace WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers()
-                .ConfigureApiBehaviorOptions(options => {
+            services.AddControllers(options =>
+                {
+                    options.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+                    options.ReturnHttpNotAcceptable = true;
+                    options.RespectBrowserAcceptHeader = true;
+                })
+                .ConfigureApiBehaviorOptions(options =>
+                {
                     options.SuppressModelStateInvalidFilter = true;
                     options.SuppressMapClientErrors = true;
+                })
+                .AddNewtonsoftJson(options =>
+                {
+                    options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                    options.SerializerSettings.DefaultValueHandling = DefaultValueHandling.Populate;
                 });
+
+            services.AddSingleton<IUserRepository, InMemoryUserRepository>();
+            services.AddAutoMapper(cfg =>
+            {
+                cfg.CreateMap<UserEntity, UserDto>()
+                    .ForMember(dest => dest.FullName,
+                        opt => opt.MapFrom(src => $"{src.LastName} {src.FirstName}"));
+
+                cfg.CreateMap<UserToCreate, UserEntity>();
+                cfg.CreateMap<UserToUpdate, UserEntity>();
+                cfg.CreateMap<UserEntity, UserToUpdate>();
+            }, new System.Reflection.Assembly[0]);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
