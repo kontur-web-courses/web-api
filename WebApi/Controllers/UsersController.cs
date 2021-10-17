d605f3bb7ea72fbcbc559ed57e72c4640e7e009d
Гордиенко Артем Андреевич <gor.aa@skbkontur.ru>
2021-10-17 16:18:53 +0500

    task: 1

--- WebApi/Controllers/UsersController.cs
+++ WebApi/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using AutoMapper;
 using Game.Domain;
 using Microsoft.AspNetCore.Mvc;
 using WebApi.Models;
@@ -6,18 +7,27 @@ using WebApi.Models;
 namespace WebApi.Controllers
 {
     [Route("api/[controller]")]
+    [Produces("application/json", "application/xml")]
     [ApiController]
     public class UsersController : Controller
     {
+        private IUserRepository userRepository;
+        private IMapper mapper;
         // Чтобы ASP.NET положил что-то в userRepository требуется конфигурация
-        public UsersController(IUserRepository userRepository)
+        public UsersController(IUserRepository userRepository, IMapper mapper)
         {
+            this.userRepository = userRepository;
+            this.mapper = mapper;
         }
 
         [HttpGet("{userId}")]
         public ActionResult<UserDto> GetUserById([FromRoute] Guid userId)
         {
-            throw new NotImplementedException();
+            var src = userRepository.FindById(userId);
+            if (src == null)
+                return NotFound();
+            var user = mapper.Map<UserEntity, UserDto>(src);
+            return Ok(user);
         }
 
         [HttpPost]
--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -1,7 +1,13 @@
+using System.Security;
+using AutoMapper;
+using Game.Domain;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using WebApi.Models;
 
 namespace WebApi
 {
@@ -22,6 +28,19 @@ namespace WebApi
                     options.SuppressModelStateInvalidFilter = true;
                     options.SuppressMapClientErrors = true;
                 });
+            services.AddSingleton<IUserRepository, InMemoryUserRepository>();
+            services.AddControllers(options =>
+                {
+                    options.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+                    options.ReturnHttpNotAcceptable = true;
+                    options.RespectBrowserAcceptHeader = true;
+                })
+                .ConfigureApiBehaviorOptions(options => { });
+            services.AddAutoMapper(cfg =>
+            {
+                cfg.CreateMap<UserEntity, UserDto>()
+                    .ForMember(dest => dest.FullName, opt => opt.MapFrom(c => $"{c.LastName} {c.FirstName}"));
+            }, new System.Reflection.Assembly[0]);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
