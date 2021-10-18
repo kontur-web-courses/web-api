365f5d9400aae379c610c6a5e43b348776812191
kryaazh <57125074+kryaazh@users.noreply.github.com>
2021-10-18 17:37:29 +0500

    Task 5—8 done

--- WebApi/Controllers/UsersController.cs
+++ WebApi/Controllers/UsersController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using AutoMapper;
 using Game.Domain;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Newtonsoft.Json;
 using WebApi.Models;
 
 namespace WebApi.Controllers
@@ -13,15 +15,21 @@ namespace WebApi.Controllers
     [ApiController]
     public class UsersController : Controller
     {
+        private readonly LinkGenerator linkGenerator;
+
+        private readonly IMapper mapper;
+
         // Чтобы ASP.NET положил что-то в userRepository требуется конфигурация
         private readonly IUserRepository userRepository;
-        private readonly IMapper mapper;
-        public UsersController(IUserRepository userRepository, IMapper mapper)
+
+        public UsersController(IUserRepository userRepository, IMapper mapper, LinkGenerator linkGenerator)
         {
             this.userRepository = userRepository;
             this.mapper = mapper;
+            this.linkGenerator = linkGenerator;
         }
 
+        [HttpHead("{userId}", Name = nameof(GetUserById))]
         [HttpGet("{userId}", Name = nameof(GetUserById))]
         [Produces("application/json", "application/xml")]
         public ActionResult<UserDto> GetUserById([FromRoute] Guid userId)
@@ -40,19 +48,19 @@ namespace WebApi.Controllers
         {
             if (user == null)
                 return BadRequest();
-            
+
             if (string.IsNullOrEmpty(user.Login) || !user.Login.All(char.IsLetterOrDigit))
-                ModelState.AddModelError(nameof(UserToCreate.Login),"Логин должен содержать только буквы или цифры");
+                ModelState.AddModelError(nameof(UserToCreate.Login), "Логин должен содержать только буквы или цифры");
 
             if (!ModelState.IsValid)
                 return UnprocessableEntity(ModelState);
-            
+
             var userEntity = mapper.Map<UserEntity>(user);
             var createdUserEntity = userRepository.Insert(userEntity);
-            
+
             return CreatedAtRoute(
                 nameof(GetUserById),
-                new { userId = createdUserEntity.Id},
+                new {userId = createdUserEntity.Id},
                 createdUserEntity.Id);
         }
 
@@ -68,15 +76,13 @@ namespace WebApi.Controllers
 
             var userEntity = new UserEntity(userId);
             mapper.Map(user, userEntity);
-            userRepository.UpdateOrInsert(userEntity, out bool isInserted);
+            userRepository.UpdateOrInsert(userEntity, out var isInserted);
 
             if (isInserted)
-            {
                 return CreatedAtRoute(
                     nameof(GetUserById),
                     new {userId = userEntity.Id},
                     userEntity.Id);
-            }
 
             return NoContent();
         }
@@ -87,7 +93,7 @@ namespace WebApi.Controllers
         {
             if (patchDoc == null)
                 return BadRequest();
-            
+
             var userFromRepository = userRepository.FindById(userId);
             if (userFromRepository == null)
                 return NotFound();
@@ -104,6 +110,60 @@ namespace WebApi.Controllers
             userRepository.Update(userFromRepository);
 
             return NoContent();
+        }
+
+
+        [HttpDelete("{userId}")]
+        [Produces("application/json", "application/xml")]
+        public IActionResult DeleteUser([FromRoute] Guid userId)
+        {
+            var userFromRepository = userRepository.FindById(userId);
+            if (userFromRepository == null)
+                return NotFound();
+
+            userRepository.Delete(userId);
+            return NoContent();
+        }
+
+        [HttpGet(Name = nameof(GetUsers))]
+        [Produces("application/json", "application/xml")]
+        public ActionResult<IEnumerable<UserDto>> GetUsers([FromQuery] int pageNumber=1, [FromQuery] int pageSize=10)
+        {
+            pageNumber = Math.Max(pageNumber, 1);
+            pageSize = Math.Min(Math.Max(pageSize, 1), 20);
+
+            var pageList = userRepository.GetPage(pageNumber, pageSize);
+
+            var paginationHeader = new
+            {
+                previousPageLink = pageList.HasPrevious ? CreateGetUsersUri(pageList.CurrentPage - 1, pageList.PageSize) : null,
+                nextPageLink = pageList.HasNext ? CreateGetUsersUri(pageList.CurrentPage + 1, pageList.PageSize) : null,
+                totalCount = pageList.TotalCount,
+                pageSize = pageList.PageSize,
+                currentPage = pageList.CurrentPage,
+                totalPages = pageList.TotalPages
+            };
+            Response.Headers.Add("X-Pagination", JsonConvert.SerializeObject(paginationHeader));
+
+            return Ok(pageList);
+        }
+        
+        [HttpOptions(Name = nameof(GetOptions))]
+        [Produces("application/json", "application/xml")]
+        public ActionResult GetOptions()
+        {
+            Response.Headers.Add("Allow", "GET, POST, OPTIONS");
+            return Ok();
+        }
+
+        private string CreateGetUsersUri(int pageNumber, int pageSize)
+        {
+            return linkGenerator.GetUriByRouteValues(HttpContext, nameof(GetUsers),
+                new
+                {
+                    pageNumber,
+                    pageSize
+                });
         }
     }
 }
