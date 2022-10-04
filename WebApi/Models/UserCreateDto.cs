42f366b589738141ba19a73dd570a38a002b9e98
udarenienao <72667318+udarenienao@users.noreply.github.com>
2022-10-04 17:49:29 +0500

    done 1-8
    Соколов Мизурова

---
+++ WebApi/Models/UserCreateDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Models
+{
+    public class UserCreateDto
+    {
+        [Required]
+        public string Login { get; set; }
+        [DefaultValue("John")]
+        public string FirstName { get; set; }
+        [DefaultValue("Doe")]
+        public string LastName { get; set; }
+    }
+}
--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -1,7 +1,13 @@
+using AutoMapper;
+using Game.Domain;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using WebApi.Models;
 
 namespace WebApi
 {
@@ -17,11 +23,32 @@ namespace WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers()
+            services.AddControllers(options =>
+                {
+                    options.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+                    options.ReturnHttpNotAcceptable = true;
+                    options.RespectBrowserAcceptHeader = true;
+                })
                 .ConfigureApiBehaviorOptions(options => {
                     options.SuppressModelStateInvalidFilter = true;
                     options.SuppressMapClientErrors = true;
-                });
+                })
+                .AddNewtonsoftJson(options =>
+                {
+                    options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                    options.SerializerSettings.DefaultValueHandling = DefaultValueHandling.Populate;
+                });;
+                
+            services.AddSingleton<IUserRepository, InMemoryUserRepository>();
+            services.AddAutoMapper(cfg =>
+            {
+                cfg.CreateMap<UserEntity, UserDto>()
+                    .ForMember(dst => dst.FullName, opt => opt.MapFrom(src => $"{src.LastName} {src.FirstName}"));
+                cfg.CreateMap<UserCreateDto, UserEntity>();
+                cfg.CreateMap<UserUpdateDto, UserEntity>();
+                cfg.CreateMap<UserEntity, UserUpdateDto>();
+            }, new System.Reflection.Assembly[0]);
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
