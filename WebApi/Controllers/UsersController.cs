5e60dbe18079e440e27148772fdd8bf59159f604
git-ethan-mars <73284049+git-ethan-mars@users.noreply.github.com>
2022-10-04 17:47:49 +0500

    First commit

--- WebApi/Controllers/UsersController.cs
+++ WebApi/Controllers/UsersController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Game.Domain;
 using Microsoft.AspNetCore.Mvc;
 using WebApi.Models;
@@ -10,14 +10,32 @@ namespace WebApi.Controllers
     public class UsersController : Controller
     {
         // Чтобы ASP.NET положил что-то в userRepository требуется конфигурация
+        public IUserRepository userRepository;
+        
         public UsersController(IUserRepository userRepository)
         {
+            this.userRepository = userRepository;
         }
 
         [HttpGet("{userId}")]
+        [Produces("application/json", "application/xml")]
         public ActionResult<UserDto> GetUserById([FromRoute] Guid userId)
         {
-            throw new NotImplementedException();
+            var userEntity = userRepository.FindById(userId);
+            if(userEntity is null)
+            {
+                return NotFound();
+            }
+
+            var dto = new UserDto()
+            {
+                Id = userEntity.Id,
+                FullName = $"{userEntity.LastName} {userEntity.FirstName}",
+                GamesPlayed = userEntity.GamesPlayed,
+                CurrentGameId = userEntity.CurrentGameId,
+                Login = userEntity.Login
+            };
+            return Ok(dto);
         }
 
         [HttpPost]
--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -1,5 +1,8 @@
+using Game.Domain;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -11,13 +14,24 @@ namespace WebApi
         {
             Configuration = configuration;
         }
-
+        
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers()
+
+            services.AddSingleton<IUserRepository, InMemoryUserRepository>();
+            services.AddControllers(options =>
+                {
+                // Этот OutputFormatter позволяет возвращать данные в XML, если требуется.
+                options.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+                // Эта настройка позволяет отвечать кодом 406 Not Acceptable на запросы неизвестных форматов.
+                options.ReturnHttpNotAcceptable = true;
+                // Эта настройка приводит к игнорированию заголовка Accept, когда он содержит */*
+                // Здесь она нужна, чтобы в этом случае ответ возвращался в формате JSON
+                options.RespectBrowserAcceptHeader = true;
+                })
                 .ConfigureApiBehaviorOptions(options => {
                     options.SuppressModelStateInvalidFilter = true;
                     options.SuppressMapClientErrors = true;
