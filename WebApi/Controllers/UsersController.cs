80d90907124a558f06a6180ab617a243598f9f73
SergeyBirukov <birukovsergej121@gmail.com>
2022-10-10 18:52:34 +0500

    1-2

--- WebApi/Controllers/UsersController.cs
+++ WebApi/Controllers/UsersController.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using AutoMapper;
 using Game.Domain;
 using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
 using WebApi.Models;
 
 namespace WebApi.Controllers
@@ -10,20 +14,61 @@ namespace WebApi.Controllers
     public class UsersController : Controller
     {
         // Чтобы ASP.NET положил что-то в userRepository требуется конфигурация
-        public UsersController(IUserRepository userRepository)
+        private IUserRepository userRepository;
+        private IMapper mapper;
+        public UsersController(IUserRepository userRepository, IMapper mapper)
         {
+            this.userRepository = userRepository;
+            this.mapper = mapper;
         }
 
-        [HttpGet("{userId}")]
+        [HttpGet("{userId}", Name = nameof(GetUserById))]
+        [Produces("application/json", "application/xml")]
         public ActionResult<UserDto> GetUserById([FromRoute] Guid userId)
         {
-            throw new NotImplementedException();
+            var src = userRepository.FindById(userId);
+            if (src is null)
+            {
+                return NotFound();
+            }
+            var user = mapper.Map<UserDto>(src);
+            return user;
         }
 
         [HttpPost]
-        public IActionResult CreateUser([FromBody] object user)
+        [Produces("application/json", "application/xml")]
+        public IActionResult CreateUser([FromBody] UserInfo user)
         {
-            throw new NotImplementedException();
+            if (user is null)
+            {
+                return BadRequest();
+            }
+            if (!IsLoginValid(user.Login))
+            {
+                ModelState.AddModelError("Login", "Login should contain only letters or digits");
+                return UnprocessableEntity(ModelState);
+            };
+            var createdUserEntity = mapper.Map<UserEntity>(user);
+            createdUserEntity = userRepository.Insert(createdUserEntity);
+            var result = CreatedAtRoute(
+                nameof(GetUserById),
+                new { userId = createdUserEntity.Id },
+                createdUserEntity.Id);
+            return result;
+
         }
+
+        private bool IsLoginValid(string login)
+        {
+            return login != null && login.All(char.IsLetterOrDigit);
+        }
+    }
+
+    public class UserInfo
+    {
+        [Required]
+        public string Login { get; set; }
+        public string FirstName{ get; set; }
+        public string LastName{ get; set; }
     }
 }
--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -1,7 +1,14 @@
+using AutoMapper;
+using Game.Domain;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
+using WebApi.Controllers;
+using WebApi.Models;
+using WebApi.Samples;
 
 namespace WebApi
 {
@@ -17,11 +24,32 @@ namespace WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers()
+            services.AddControllers(options =>
+                {
+                    // Этот OutputFormatter позволяет возвращать данные в XML, если требуется.
+                    options.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+                    // Эта настройка позволяет отвечать кодом 406 Not Acceptable на запросы неизвестных форматов.
+                    options.ReturnHttpNotAcceptable = true;
+                    // Эта настройка приводит к игнорированию заголовка Accept, когда он содержит */*
+                    // Здесь она нужна, чтобы в этом случае ответ возвращался в формате JSON
+                    options.RespectBrowserAcceptHeader = true;
+                })
                 .ConfigureApiBehaviorOptions(options => {
                     options.SuppressModelStateInvalidFilter = true;
                     options.SuppressMapClientErrors = true;
-                });
+                }).AddNewtonsoftJson(options =>
+                {
+                    options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                });;
+            services.AddSingleton<IUserRepository, InMemoryUserRepository>();
+            services.AddAutoMapper(cfg =>
+            {
+                cfg.CreateMap<UserEntity, UserDto>().ForMember(
+                    dest => dest.FullName,
+                    opt =>
+                        opt.MapFrom(src => $"{src.LastName} {src.FirstName}"));
+                cfg.CreateMap<UserInfo, UserEntity>();
+            }, new System.Reflection.Assembly[0]);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
