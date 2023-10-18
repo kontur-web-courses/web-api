dc1929c5d9efdbdb2a2b9502aaee367872a66f41
SBB <me.mashkin7@gmail.com>
2023-10-18 20:12:33 +0500

    Task1 without AutoMapper

--- WebApi/Controllers/UsersController.cs
+++ WebApi/Controllers/UsersController.cs
@@ -9,15 +9,27 @@ namespace WebApi.Controllers
     [ApiController]
     public class UsersController : Controller
     {
+        private IUserRepository userRepository;
         // Чтобы ASP.NET положил что-то в userRepository требуется конфигурация
         public UsersController(IUserRepository userRepository)
         {
+            this.userRepository = userRepository;
         }
 
         [HttpGet("{userId}")]
+        [Produces("application/json", "application/xml")]
         public ActionResult<UserDto> GetUserById([FromRoute] Guid userId)
         {
-            throw new NotImplementedException();
+            var userDto = new UserDto();
+            var userEntity = userRepository.FindById(userId);
+            if (userEntity is null)
+                return NotFound();
+            userDto.Id = userEntity.Id;
+            userDto.GamesPlayed = userEntity.GamesPlayed;
+            userDto.CurrentGameId = userEntity.CurrentGameId;
+            userDto.Login = userEntity.Login;
+            userDto.FullName = userEntity.FirstName + userEntity.LastName;
+            return Ok(userDto);
         }
 
         [HttpPost]
--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -1,5 +1,8 @@
+using AutoMapper;
+using Game.Domain;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -17,6 +20,18 @@ namespace WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IUserRepository, InMemoryUserRepository>();
+            services.AddControllers(options =>
+                {
+                    // Этот OutputFormatter позволяет возвращать данные в XML, если требуется.
+                    options.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+                    // Эта настройка позволяет отвечать кодом 406 Not Acceptable на запросы неизвестных форматов.
+                    options.ReturnHttpNotAcceptable = true;
+                    // Эта настройка приводит к игнорированию заголовка Accept, когда он содержит */*
+                    // Здесь она нужна, чтобы в этом случае ответ возвращался в формате JSON
+                    options.RespectBrowserAcceptHeader = true;
+                });
+            
             services.AddControllers()
                 .ConfigureApiBehaviorOptions(options => {
                     options.SuppressModelStateInvalidFilter = true;
