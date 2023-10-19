a5e9ebafcb94e537864f222c8887f58e195206f8
SBB <me.mashkin7@gmail.com>
2023-10-19 12:57:24 +0500

    Task 7

--- WebApi/Controllers/UsersController.cs
+++ WebApi/Controllers/UsersController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using AutoMapper;
 using Game.Domain;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Newtonsoft.Json;
 using WebApi.Models;
 
 namespace WebApi.Controllers
@@ -16,11 +19,14 @@ namespace WebApi.Controllers
 
         private readonly IMapper mapper;
 
+        private readonly LinkGenerator linkGenerator;
+
         // Чтобы ASP.NET положил что-то в userRepository требуется конфигурация
-        public UsersController(IUserRepository userRepository, IMapper mapper)
+        public UsersController(IUserRepository userRepository, IMapper mapper, LinkGenerator linkGenerator)
         {
             this.mapper = mapper;
             this.userRepository = userRepository;
+            this.linkGenerator = linkGenerator;
         }
 
         [HttpGet("{userId}", Name = nameof(GetUserById))]
@@ -59,7 +65,7 @@ namespace WebApi.Controllers
             if (userUpdateDto is null || userId == Guid.Empty)
                 return BadRequest();
             if (!ModelState.IsValid) return UnprocessableEntity(ModelState);
-            
+
             var userEntity = new UserEntity(userId);
             mapper.Map(userUpdateDto, userEntity);
             userRepository.UpdateOrInsert(userEntity, out var isInserted);
@@ -76,10 +82,10 @@ namespace WebApi.Controllers
         public IActionResult PartiallyUpdateUser([FromRoute] Guid userId, [FromBody] JsonPatchDocument<UpdateDto> patchDoc)
         {
             if (patchDoc is null) return BadRequest();
-            
+
             var userEntity = userRepository.FindById(userId);
             if (userEntity is null) return NotFound();
-            
+
             var updateDto = mapper.Map<UpdateDto>(userEntity);
             patchDoc.ApplyTo(updateDto, ModelState);
             if (!TryValidateModel(updateDto)) return UnprocessableEntity(ModelState);
@@ -95,6 +101,44 @@ namespace WebApi.Controllers
                 return NotFound();
             userRepository.Delete(userId);
             return NoContent();
+        }
+
+        [HttpGet(Name = nameof(GetUsers))]
+        [Produces("application/json", "application/xml")]
+        public ActionResult<UserDto> GetUsers([FromQuery] int? pageNumber, [FromQuery] int? pageSize)
+        {
+            pageNumber ??= 1;
+            if (pageNumber < 1) pageNumber = 1;
+
+            pageSize = (int) (pageSize switch
+            {
+                null => 10,
+                < 1 => 1,
+                > 20 => 20,
+                _ => pageSize
+            });
+
+            var pageList = userRepository.GetPage(pageNumber.Value, pageSize.Value);
+            string previousPageLink = null;
+            string nextPageLink = null;
+            var previousPageNumber = pageNumber - 1;
+            var nextPageNumber = pageNumber + 1;
+            if (pageList.HasPrevious)
+                previousPageLink = linkGenerator.GetUriByRouteValues(HttpContext, nameof(GetUsers), new {pageSize, previousPageNumber});
+            if (pageList.HasNext)
+                nextPageLink = linkGenerator.GetUriByRouteValues(HttpContext, nameof(GetUsers), new {pageSize, nextPageNumber});
+
+            var paginationHeader = new
+            {
+                previousPageLink,
+                nextPageLink,
+                totalCount = pageList.TotalCount,
+                pageSize,
+                currentPage = pageNumber,
+                totalPages = pageList.TotalPages
+            };
+            Response.Headers.Add("X-Pagination", JsonConvert.SerializeObject(paginationHeader));
+            return Ok(paginationHeader);
         }
     }
 }
