b37d469c6f956a45f5730f260fcc0300cd9e9c51
Иванаев Станислав Владимирович <ivanaev@skbkontur.ru>
2023-11-30 12:29:40 +0500

    update project to .net8, update tests to .net 8, fix controllers mapping

--- WebApi.MinimalApi/Program.cs
+++ WebApi.MinimalApi/Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseUrls("https://localhost:5001;http://localhost:5000");
+builder.WebHost.UseEnvironment("Development");
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapControllers();
 
 app.Run();
