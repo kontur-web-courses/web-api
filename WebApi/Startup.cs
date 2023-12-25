c7d23a66a194366e34cf0433c2a81b318b1b77bd
PavelLovygin <lovyginpavel0@gmail.com>
2023-12-25 17:14:35 +0500

    task 1

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -1,7 +1,13 @@
+using System;
+using System.Reflection;
+using AutoMapper;
+using Game.Domain;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using WebApi.Models;
 
 namespace WebApi
 {
@@ -17,11 +23,21 @@ namespace WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers()
-                .ConfigureApiBehaviorOptions(options => {
-                    options.SuppressModelStateInvalidFilter = true;
-                    options.SuppressMapClientErrors = true;
-                });
+            services.AddSingleton<IUserRepository, InMemoryUserRepository>();
+            services.AddControllers(options =>
+            {
+                options.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+                options.ReturnHttpNotAcceptable = true;
+                options.RespectBrowserAcceptHeader = true;
+            });
+            services.AddAutoMapper(cfg =>
+            {
+                cfg.CreateMap<UserEntity, UserDto>()
+                    .ForMember(
+                        dest => dest.FullName,
+                        opt => opt.MapFrom(src => $"{src.LastName} {src.FirstName}")
+                    );
+            }, Array.Empty<Assembly>());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
