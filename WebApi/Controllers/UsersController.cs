fed75b7a2f77608379ffb3dd36a97ffb169d6547
PavelLovygin <lovyginpavel0@gmail.com>
2023-12-25 17:43:05 +0500

    complete task

--- WebApi/Controllers/UsersController.cs
+++ WebApi/Controllers/UsersController.cs
@@ -1,6 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
 using Game.Domain;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Newtonsoft.Json;
 using WebApi.Models;
 
 namespace WebApi.Controllers
@@ -9,21 +15,141 @@ namespace WebApi.Controllers
     [ApiController]
     public class UsersController : Controller
     {
-        // Чтобы ASP.NET положил что-то в userRepository требуется конфигурация
-        public UsersController(IUserRepository userRepository)
+        private readonly IUserRepository userRepository;
+        private readonly IMapper mapper;
+        private readonly LinkGenerator linkGenerator;
+
+        public UsersController(IUserRepository userRepository, IMapper mapper, LinkGenerator linkGenerator)
         {
+            this.userRepository = userRepository;
+            this.mapper = mapper;
+            this.linkGenerator = linkGenerator;
         }
 
-        [HttpGet("{userId}")]
+        [HttpGet("{userId}", Name = nameof(GetUserById))]
+        [HttpHead("{userId}")]
+        [Produces("application/json", "application/xml")]
         public ActionResult<UserDto> GetUserById([FromRoute] Guid userId)
         {
-            throw new NotImplementedException();
+            var user = userRepository.FindById(userId);
+
+            if (user == null)
+                return NotFound();
+
+            return Ok(mapper.Map<UserDto>(user));
         }
 
         [HttpPost]
-        public IActionResult CreateUser([FromBody] object user)
+        [Produces("application/json", "application/xml")]
+        public IActionResult CreateUser([FromBody] UserToCreateDto userDto)
         {
-            throw new NotImplementedException();
+            if (userDto == null)
+                return BadRequest();
+
+            if (!ModelState.IsValid)
+                return UnprocessableEntity(ModelState);
+
+            if (!userDto.Login.All(char.IsLetterOrDigit))
+            {
+                ModelState.AddModelError("Login", "Login should contain only letters or digits");
+                return UnprocessableEntity(ModelState);
+            }
+
+            var user = mapper.Map<UserEntity>(userDto);
+            user = userRepository.Insert(user);
+
+            return CreatedAtRoute(
+                nameof(GetUserById),
+                new { userId = user.Id },
+                user.Id);
+        }
+
+        [HttpPut("{userId}")]
+        [Produces("application/json", "application/xml")]
+        public IActionResult UpdateUser([FromBody] UserToUpdateDto userDto, [FromRoute] Guid userId)
+        {
+            if (userDto == null || userId == Guid.Empty)
+                return BadRequest();
+
+            if (!ModelState.IsValid)
+                return UnprocessableEntity(ModelState);
+
+            var user = mapper.Map(userDto, new UserEntity(userId));
+            userRepository.UpdateOrInsert(user, out var isInserted);
+
+            return isInserted ? CreatedAtRoute(nameof(GetUserById), new { userId = user.Id }, user.Id) : NoContent();
+        }
+
+        [HttpPatch("{userId}")]
+        [Produces("application/json", "application/xml")]
+        public IActionResult PartiallyUpdateUser([FromBody] JsonPatchDocument<UserToUpdateDto> patchDoc, [FromRoute] Guid userId)
+        {
+            if (patchDoc == null)
+                return BadRequest();
+
+            var user = userRepository.FindById(userId);
+            if (user == null)
+                return NotFound();
+
+            var userDto = mapper.Map<UserToUpdateDto>(user);
+            patchDoc.ApplyTo(userDto, ModelState);
+
+            if (!TryValidateModel(userDto))
+                return UnprocessableEntity(ModelState);
+
+            user = mapper.Map<UserEntity>(userDto);
+            userRepository.Update(user);
+            return NoContent();
+        }
+
+        [HttpDelete("{userId}")]
+        [Produces("application/json", "application/xml")]
+        public IActionResult DeleteUser([FromRoute] Guid userId)
+        {
+            var user = userRepository.FindById(userId);
+            if (user == null)
+                return NotFound();
+
+            userRepository.Delete(userId);
+            return NoContent();
+        }
+
+        [HttpGet(Name = nameof(GetUsers))]
+        [Produces("application/json", "application/xml")]
+        public ActionResult<IEnumerable<UserDto>> GetUsers([FromQuery] int? pageNumber, [FromQuery] int? pageSize)
+        {
+            var intPageNumber = pageNumber != null ? Math.Max(1, pageNumber!.Value) : 1;
+
+            var intPageSize = pageSize != null ? Math.Min(20, Math.Max(1, pageSize!.Value)) : 10;
+
+            var pageList = userRepository.GetPage(intPageNumber, intPageSize);
+
+            var paginationHeader = new
+            {
+                previousPageLink = pageList.HasPrevious ? GetUriUsers(intPageNumber - 1, intPageSize) : null,
+                nextPageLink = pageList.HasNext ? GetUriUsers(intPageNumber + 1, intPageSize) : null,
+                totalCount = pageList.TotalCount,
+                pageSize = pageList.PageSize,
+                currentPage = pageList.CurrentPage,
+                totalPages = pageList.TotalPages,
+            };
+
+            Response.Headers.Add("X-Pagination", JsonConvert.SerializeObject(paginationHeader));
+
+            return Ok(paginationHeader);
+        }
+
+        private string GetUriUsers(int pageNumber, int pageSize)
+        {
+            return linkGenerator.GetUriByRouteValues(HttpContext, nameof(GetUsers), new { pageSize, pageNumber });
+        }
+
+        [HttpOptions]
+        [Produces("application/json", "application/xml")]
+        public IActionResult Options()
+        {
+            Response.Headers.Add("Allow", "POST, GET, OPTIONS");
+            return Ok();
         }
     }
 }
