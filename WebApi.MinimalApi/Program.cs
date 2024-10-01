ae611e2f53ee682734c26e8d32edc5f205e49323
lehakurshev <lehakurshev@mail.ru>
2024-10-01 17:56:12 +0500

    Task1_GetUserByIdTests

--- WebApi.MinimalApi/Program.cs
+++ WebApi.MinimalApi/Program.cs
@@ -1,14 +1,35 @@
 using WebApi.MinimalApi.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using WebApi.MinimalApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseUrls("http://localhost:5000");
-builder.Services.AddControllers()
-    .ConfigureApiBehaviorOptions(options => {
-        options.SuppressModelStateInvalidFilter = true;
-        options.SuppressMapClientErrors = true;
-    });
 
+// Добавляем контроллеры с настройками для поддержки JSON и XML
+builder.Services.AddControllers(options =>
+{
+    // Добавляем поддержку XML
+    options.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+
+    // Возвращать код 406 Not Acceptable для неподдерживаемых форматов
+    options.ReturnHttpNotAcceptable = true;
+
+    // Игнорируем заголовок Accept, если он содержит */*
+    options.RespectBrowserAcceptHeader = true;
+})
+.ConfigureApiBehaviorOptions(options => {
+    options.SuppressModelStateInvalidFilter = true;
+    options.SuppressMapClientErrors = true;
+});
+
+// Регистрируем репозиторий пользователей в памяти
 builder.Services.AddSingleton<IUserRepository, InMemoryUserRepository>();
+
+builder.Services.AddAutoMapper(cfg =>
+{
+    cfg.CreateMap<UserEntity, UserDto>();
+}, new System.Reflection.Assembly[0]);
 
 var app = builder.Build();
 
