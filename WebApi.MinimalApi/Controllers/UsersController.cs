4f12c58cb574c77b16e493c9a902a42acc0922fc
n.aleksandrov <n.aleksandrov@skbkontur.ru>
2024-10-01 17:41:13 +0500

    Сделаны задания 1, 2, 3

--- WebApi.MinimalApi/Controllers/UsersController.cs
+++ WebApi.MinimalApi/Controllers/UsersController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Security.Cryptography.X509Certificates;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
 using WebApi.MinimalApi.Domain;
 using WebApi.MinimalApi.Models;
 
@@ -9,19 +12,62 @@ namespace WebApi.MinimalApi.Controllers;
 public class UsersController : Controller
 {
     // Чтобы ASP.NET положил что-то в userRepository требуется конфигурация
-    public UsersController(IUserRepository userRepository)
+    private IUserRepository Repository;
+    private IMapper Mapper;
+    public UsersController(IUserRepository userRepository, IMapper mapper)
     {
+        Repository = userRepository;
+        Mapper = mapper;
     }
 
-    [HttpGet("{userId}")]
+    [HttpGet("{userId}", Name = nameof(GetUserById))]
+    [Produces("application/json", "application/xml")]
     public ActionResult<UserDto> GetUserById([FromRoute] Guid userId)
     {
-        throw new NotImplementedException();
+        var user = Repository.FindById(userId);
+        if (user is null)
+            return NotFound();
+        var result = Mapper.Map<UserDto>(user);
+        return Ok(result);
     }
 
     [HttpPost]
-    public IActionResult CreateUser([FromBody] object user)
+    public IActionResult CreateUser([FromBody] AddUserDto user)
     {
-        throw new NotImplementedException();
+        var createdUserEntity = Mapper.Map<UserEntity>(user);
+        if (createdUserEntity is null)
+            return BadRequest();
+        if(createdUserEntity.Login?.All(char.IsLetterOrDigit) == false)
+            ModelState.AddModelError(nameof(createdUserEntity.Login).ToLower(), "Login must be letters or digits");
+        if (!ModelState.IsValid)
+            return UnprocessableEntity(ModelState);
+        Repository.Insert(createdUserEntity);
+        return CreatedAtRoute(
+            nameof(GetUserById),
+            new { userId = createdUserEntity.Id },
+            createdUserEntity.Id);
+    }
+
+    [HttpPut("{userId}")]
+    [Produces("application/json", "application/xml")]
+    public IActionResult UpdateUser([FromQuery] Guid userId, [FromBody] UpdateUserDto user)
+    {
+        if (user == null)
+            return BadRequest();
+        if (!ModelState.IsValid)
+            return UnprocessableEntity(ModelState);
+        if (userId == Guid.Empty)
+            return BadRequest();
+
+        var userEntity = Mapper.Map(user, new UserEntity(userId));
+
+        Repository.UpdateOrInsert(userEntity, out var isInsert);
+
+        if (isInsert)
+            return CreatedAtRoute(
+                nameof(GetUserById),
+                new {userId = userId},
+                userId);
+        return NoContent();
     }
 }
---
+++ WebApi.MinimalApi/Models/UpdateUserDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.MinimalApi.Models;
+
+public class UpdateUserDto
+{
+    [Required]
+    [RegularExpression("^[0-9\\p{L}]*$", ErrorMessage = "Login should contain only letters or digits")]
+    public string Login { get; set; }
+    [Required]
+    public string FirstName { get; set; }
+    [Required]
+    public string LastName { get; set; }
+}
