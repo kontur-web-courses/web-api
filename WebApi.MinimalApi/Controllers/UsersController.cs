dac45105f85b600121d1b6a4f11797e0da4775c3
MaxD3mage <terzi.kirill@gmail.com>
2024-10-01 17:42:01 +0500

    1,2 task completed

--- WebApi.MinimalApi/Controllers/UsersController.cs
+++ WebApi.MinimalApi/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using WebApi.MinimalApi.Domain;
 using WebApi.MinimalApi.Models;
 
@@ -8,20 +9,56 @@ namespace WebApi.MinimalApi.Controllers;
 [ApiController]
 public class UsersController : Controller
 {
+    private readonly IUserRepository _userRepository;
+    private readonly IMapper _mapper;
     // Чтобы ASP.NET положил что-то в userRepository требуется конфигурация
-    public UsersController(IUserRepository userRepository)
+    public UsersController(IUserRepository userRepository, IMapper mapper)
     {
+        _userRepository = userRepository;
+        _mapper = mapper;
     }
 
-    [HttpGet("{userId}")]
+    [HttpGet("{userId}", Name = nameof(GetUserById)) ]
+    [Produces("application/json", "application/xml")]
     public ActionResult<UserDto> GetUserById([FromRoute] Guid userId)
     {
-        throw new NotImplementedException();
+        var user = _userRepository.FindById(userId);
+        if (user == null)
+        {
+            return NotFound();
+        }
+        return Ok(_mapper.Map<UserDto>(user));
     }
 
     [HttpPost]
-    public IActionResult CreateUser([FromBody] object user)
+    public IActionResult CreateUser([FromBody] UserCreationDto? user)
     {
-        throw new NotImplementedException();
+        if (user == null)
+        {
+            return BadRequest();
+        }
+        
+        if (string.IsNullOrWhiteSpace(user.Login))
+        {
+            ModelState.AddModelError("Login", "Login is required");
+        }
+        else
+        {
+            if (user.Login.Any(c => !char.IsLetterOrDigit(c)))
+            {
+                ModelState.AddModelError("Login", "Login must consist only of letters and digits");
+            }
+        }
+        
+        if (!ModelState.IsValid)
+        {
+            return UnprocessableEntity(ModelState);
+        }
+        var createdUserEntity = _mapper.Map<UserEntity>(user);
+        _userRepository.Insert(createdUserEntity);
+        return CreatedAtRoute(
+            nameof(GetUserById),
+            new { userId = createdUserEntity.Id },
+            createdUserEntity.Id);
     }
 }
--- WebApi.MinimalApi/Models/UserDto.cs
+++ WebApi.MinimalApi/Models/UserDto.cs
@@ -1,3 +1,6 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace WebApi.MinimalApi.Models;
 
 public class UserDto
@@ -7,4 +10,16 @@ public class UserDto
     public string FullName { get; set; }
     public int GamesPlayed { get; set; }
     public Guid? CurrentGameId { get; set; }
+}
+
+public class UserCreationDto
+{
+    [Required]
+    public string Login { get; set; }
+
+    [DefaultValue("John")]
+    public string FirstName { get; set; }
+
+    [DefaultValue("Doe")]
+    public string LastName { get; set; }
 }
