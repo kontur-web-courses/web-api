b12d3675c699c56eecf2b8f3ac42d4036b826183
Бояков Михаил Владимирович <boyakov@skbkontur.ru>
2024-10-04 18:45:34 +0500

    feat: done

--- WebApi.MinimalApi/Controllers/UsersController.cs
+++ WebApi.MinimalApi/Controllers/UsersController.cs
@@ -1,27 +1,132 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
+using AutoMapper;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
 using WebApi.MinimalApi.Domain;
 using WebApi.MinimalApi.Models;
+using WebApi.MinimalApi.Models.Requests;
 
 namespace WebApi.MinimalApi.Controllers;
 
-[Route("api/[controller]")]
 [ApiController]
+[Route("api/[controller]")]
 public class UsersController : Controller
 {
+    private readonly IUserRepository userRepository;
+    private readonly IMapper mapper;
+    private readonly LinkGenerator linkGenerator;
+
     // Чтобы ASP.NET положил что-то в userRepository требуется конфигурация
-    public UsersController(IUserRepository userRepository)
+    public UsersController(IUserRepository userRepository, IMapper mapper, LinkGenerator linkGenerator)
     {
+        this.userRepository = userRepository;
+        this.mapper = mapper;
+        this.linkGenerator = linkGenerator;
     }
 
+    [HttpHead("{userId}")]
     [HttpGet("{userId}")]
+    [Produces("application/json", "application/xml")]
     public ActionResult<UserDto> GetUserById([FromRoute] Guid userId)
     {
-        throw new NotImplementedException();
+        if (HttpMethods.IsHead(Request.Method))
+            Response.Body = Stream.Null;
+        var user = userRepository.FindById(userId);
+        return user is not null ? Ok(mapper.Map<UserDto>(user)) : NotFound();
     }
 
     [HttpPost]
-    public IActionResult CreateUser([FromBody] object user)
+    [Produces("application/json", "application/xml")]
+    public IActionResult CreateUser([FromBody] CreateUserRequest request)
     {
-        throw new NotImplementedException();
+        if (request is null)
+            return BadRequest();
+        if (!request.Login?.All(char.IsAsciiLetterOrDigit) is true)
+            ModelState.AddModelError("Login", "Login should contain only digits and letters");
+        if (!ModelState.IsValid)
+            return UnprocessableEntity(ModelState);
+        var entity = mapper.Map<UserEntity>(request);
+        var result = userRepository.Insert(entity);
+        return CreatedAtAction(nameof(GetUserById), new { userId = result.Id }, result.Id);
+    }
+
+    [HttpPut("{userId}")]
+    [Produces("application/json", "application/xml")]
+    public IActionResult UpsertUser([FromRoute] Guid userId, [FromBody] UpdateUserRequest request)
+    {
+        if (request is null || userId == Guid.Empty)
+            return BadRequest();
+        if (!ModelState.IsValid)
+            return UnprocessableEntity(ModelState);
+        var entity = mapper.Map<UserEntity>(request);
+        entity.Id = userId;
+        userRepository.UpdateOrInsert(entity, out var inserted);
+        return inserted
+            ? CreatedAtAction(nameof(GetUserById), new { userId }, userId)
+            : NoContent();
+    }
+
+    [HttpPatch("{userId}")]
+    [Produces("application/json", "application/xml")]
+    public IActionResult PartiallyUpdateUser([FromRoute] Guid userId, [FromBody] JsonPatchDocument<UpdateUserRequest> patchDoc)
+    {
+        if (patchDoc is null)
+            return BadRequest();
+        var user = userRepository.FindById(userId);
+        if (user is null)
+            return NotFound();
+        var update = mapper.Map<UpdateUserRequest>(user);
+        patchDoc.ApplyTo(update, ModelState);
+        TryValidateModel(update);
+        if (!ModelState.IsValid)
+            return UnprocessableEntity(ModelState);
+        var entity = mapper.Map<UserEntity>(update);
+        entity.Id = userId;
+        userRepository.Update(entity);
+        return NoContent();
+    }
+
+    [HttpDelete("{userId}")]
+    [Produces("application/json", "application/xml")]
+    public IActionResult DeleteUser([FromRoute] Guid userId)
+    {
+        if (userRepository.FindById(userId) is null)
+            return NotFound();
+        userRepository.Delete(userId);
+        return NoContent();
+    }
+
+    [HttpGet(Name = "get-pages")]
+    [Produces("application/json", "application/xml")]
+    public ActionResult<IEnumerable<UserDto>> GetUsers(
+        [FromQuery] int pageNumber = 1,
+        [FromQuery] int pageSize = 10)
+    {
+        pageSize = Math.Clamp(pageSize,1, 20);
+        if (pageNumber < 1) pageNumber = 1;
+        var totalCount = userRepository.GetPage(1, int.MaxValue).Count;
+        var paginationInfo = new
+        {
+            previousPageLink = pageNumber == 1 
+                ? null
+                : linkGenerator.GetUriByName(HttpContext, "get-pages", new { pageNumber = pageNumber - 1, pageSize }),
+            nextPageLink = linkGenerator.GetUriByName(HttpContext, "get-pages", new { pageNumber = pageNumber + 1, pageSize }),
+            totalCount,
+            pageSize,
+            currentPage = pageNumber,
+            totalPages = Math.Ceiling(1f * totalCount / pageSize),
+        };
+        Response.Headers.Add("X-Pagination", JsonSerializer.Serialize(paginationInfo));
+        var entities = userRepository.GetPage(pageNumber, pageSize);
+        return Ok(mapper.Map<IEnumerable<UserDto>>(entities));
+    }
+
+    [HttpOptions]
+    public IActionResult Options()
+    {
+        var allowedMethods = new[] { HttpMethods.Get, HttpMethods.Options, HttpMethods.Post };
+        Response.Headers.Add("Allow", string.Join(", ", allowedMethods));
+        return Ok();
     }
 }
--- WebApi.MinimalApi/Program.cs
+++ WebApi.MinimalApi/Program.cs
@@ -1,13 +1,45 @@
+using System.Reflection;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using WebApi.MinimalApi.Domain;
+using WebApi.MinimalApi.Models;
+using WebApi.MinimalApi.Models.Requests;
+
 var builder = WebApplication.CreateBuilder(args);
-builder.WebHost.UseUrls("http://localhost:5000");
-builder.Services.AddControllers()
-    .ConfigureApiBehaviorOptions(options => {
+builder.WebHost.UseUrls("http://localhost:5001");
+builder.Services.AddControllers(options =>
+    {
+        options.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+        options.ReturnHttpNotAcceptable = true;
+        options.RespectBrowserAcceptHeader = true;
+    })
+    .ConfigureApiBehaviorOptions(options =>
+    {
         options.SuppressModelStateInvalidFilter = true;
         options.SuppressMapClientErrors = true;
+    })
+    .AddNewtonsoftJson(options =>
+    {
+        options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+        options.SerializerSettings.DefaultValueHandling = DefaultValueHandling.Populate;
     });
+builder.Services.AddSingleton<IUserRepository, InMemoryUserRepository>();
+builder.Services.AddAutoMapper(cfg =>
+{
+    cfg.CreateMap<UserEntity, UpdateUserRequest>();
+    cfg.CreateMap<UserEntity, UserDto>()
+        .ForMember(dto => dto.FullName,
+            opt => opt.MapFrom(user => $"{user.LastName} {user.FirstName}"));
+    cfg.CreateMap<CreateUserRequest, UserEntity>();
+    cfg.CreateMap<UpdateUserRequest, UserEntity>();
+}, Array.Empty<Assembly>());
 
 var app = builder.Build();
 
+app.UseRouting();
+app.MapGet("/test", () => "Test");
 app.MapControllers();
 
 app.Run();
