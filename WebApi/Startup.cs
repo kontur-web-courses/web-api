1a2ffbea2165749c4b616bb21cd3576033d7e8b4
nmzgnv <mozganov@tochka.com>
2024-10-07 20:48:25 +0500

    fix: remove comment

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -57,8 +57,6 @@ namespace WebApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //app.UseDeveloperExceptionPage();
-
             app.UseHttpsRedirection();
 
             app.UseRouting();
