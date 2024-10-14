d4ae0a6c4ca82c5ae8c722609f564b06d3eb4824
vVladEr <vlader.2004.ekb@gmail.com>
2024-10-14 10:38:07 +0500

    task 8

--- WebApi.MinimalApi/Controllers/UsersController.cs
+++ WebApi.MinimalApi/Controllers/UsersController.cs
@@ -137,4 +137,12 @@ public class UsersController : Controller
         Response.Headers.Add("X-Pagination", JsonConvert.SerializeObject(paginationHeader));
         return Ok(users);
     }
+
+    [HttpOptions]
+    [Produces("application/json", "application/xml")]
+    public IActionResult GetOptions()
+    {
+        Response.Headers.Add("Allow", new[] { "POST", "GET", "OPTIONS" });
+        return Ok();
+    }
 }
